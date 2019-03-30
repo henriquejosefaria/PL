ca16e108f51126eb9966361cc9a7d4428bd8f31d
Marquinhos87 <a81826@alunos.uminho.pt>
2019-03-30 22:14:37 +0000

    ficheiros finais

---
+++ createFiles.c
@@ -0,0 +1,35 @@
+#include <fcntl.h>
+
+int main(int argc, char const *argv[])
+{
+	open("a.html", O_CREAT | O_TRUNC, 0666);
+	open("b.html", O_CREAT | O_TRUNC, 0666);
+	open("c.html", O_CREAT | O_TRUNC, 0666);
+	open("d.html", O_CREAT | O_TRUNC, 0666);
+	open("e.html", O_CREAT | O_TRUNC, 0666);
+	open("f.html", O_CREAT | O_TRUNC, 0666);
+	open("g.html", O_CREAT | O_TRUNC, 0666);
+	open("h.html", O_CREAT | O_TRUNC, 0666);
+	open("i.html", O_CREAT | O_TRUNC, 0666);
+	open("j.html", O_CREAT | O_TRUNC, 0666);
+	open("k.html", O_CREAT | O_TRUNC, 0666);
+	open("l.html", O_CREAT | O_TRUNC, 0666);
+	open("m.html", O_CREAT | O_TRUNC, 0666);
+	open("n.html", O_CREAT | O_TRUNC, 0666);
+	open("o.html", O_CREAT | O_TRUNC, 0666);
+	open("p.html", O_CREAT | O_TRUNC, 0666);
+	open("q.html", O_CREAT | O_TRUNC, 0666);
+	open("r.html", O_CREAT | O_TRUNC, 0666);
+	open("s.html", O_CREAT | O_TRUNC, 0666);
+	open("t.html", O_CREAT | O_TRUNC, 0666);
+	open("u.html", O_CREAT | O_TRUNC, 0666);
+	open("v.html", O_CREAT | O_TRUNC, 0666);
+	open("w.html", O_CREAT | O_TRUNC, 0666);
+	open("x.html", O_CREAT | O_TRUNC, 0666);
+	open("y.html", O_CREAT | O_TRUNC, 0666);
+	open("z.html", O_CREAT | O_TRUNC, 0666);
+	open("dictionary.txt", O_CREAT | O_TRUNC, 0666);
+	open("dic.txt", O_CREAT | O_TRUNC, 0666);
+	int i = open("indice.html", O_CREAT | O_TRUNC, 0666); 
+	return 0;
+}
--- makefile
+++ makefile
@@ -2,13 +2,28 @@ CC=gcc
 CFLAGS= $(shell pkg-config --cflags glib-2.0)
 LFLAGS= $(shell pkg-config --libs glib-2.0)
 
-all: clean tp1
+all: clean files tp1 last
+
+.PHONY: last
+
+last: 
+	iconv -c -s -f utf8 -t utf8 dictionary.txt > dic.txt
+
+files: createFiles.c
+	gcc createFiles.c -o files
+	./files
+	rm files
 
 lex.yy.c: 1tp1.fl
 	flex -8 -f 1tp1.fl
 
 tp1: lex.yy.c
-	$(CC) $(CFLAGS) lex.yy.c -o tp1 $(LFLAGS)  
+	$(CC) $(CFLAGS) lex.yy.c -o tp1 $(LFLAGS)
+	time ./tp1 ptwiki-20190220-pttit_id.txt ptwiki-latest-langlinks.sql
+	rm tp1
+
+alternativa: clean files lex.yy.c
+	$(CC) $(CFLAGS) lex.yy.c -o tp1 $(LFLAGS)
 
 .PHONY: clean
 
@@ -16,9 +31,6 @@ clean:
 	rm -f lex.yy.c
 	rm -f tp1
 	rm -f output.txt
-
-install: unistall tp1
-	sudo cp tp1 /usr/local/bin
-
-unistall:
-	sudo rm -f /usr/local/bin/tp1
+	rm -f *.html
+	rm -f dic.txt
+	rm -f dictionary.txt
